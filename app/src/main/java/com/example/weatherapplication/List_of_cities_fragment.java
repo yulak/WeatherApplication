7ff00a45bc7786a4547dc51dff042b801a9143fc
yulak <dyulakov@mail.ru>
2020-09-03 00:23:29 +0400

    реализовал с помощью двух одинаковых методов, но разными способами Проблема в том, что один фрагмент меняется, но при этом не удаляется а наносится поверх старого.

--- app/src/main/java/com/example/weatherapplication/List_of_cities_fragment.java
+++ app/src/main/java/com/example/weatherapplication/List_of_cities_fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,7 @@ public class List_of_cities_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_of_cities, container, false);
+
+        return inflater.inflate(R.layout.fragment_list_of_cities, null);
     }
 }
--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -7,17 +7,40 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    List_of_cities_fragment fragCity;
+    Settings_fragment fragSettings;
+    FragmentTransaction fTrans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragCity = new List_of_cities_fragment();
+        fragSettings = new Settings_fragment();
+
     }
 
-    public void Change (View view){
+    public void Change(View view){
+        fTrans = getSupportFragmentManager().beginTransaction();
+        switch (view.getId()){
+            case R.id.city:
+                fTrans.add(R.id.fragment_main, fragCity);
+                break;
+            case R.id.settings:
+                fTrans.replace(R.id.fragment_main, fragSettings);
+            default:
+                break;
+        }
+        fTrans.commit();
+    }
+
+    /*public void Change (View view){
         Fragment fragment = null;
+
 
         switch (view.getId()){
             case R.id.city: //нажимая эту кнопку вылезут города
@@ -27,10 +50,9 @@ public class MainActivity extends AppCompatActivity {
                 fragment = new Settings_fragment();
                 break;
         }
-
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.fragment_main, fragment);
+        ft.replace(R.id.fragment_main, fragment);
         ft.commit();
-    }
+    }*/
 }
